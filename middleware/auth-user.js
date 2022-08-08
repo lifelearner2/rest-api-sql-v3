be939cb603c7ee799fea0b8ffd15e875a0f15f32
lifelearner2 <105907575+lifelearner2@users.noreply.github.com>
2022-08-08 00:46:37 -0700

    Update auth-user.js
    working on authentication

--- middleware/auth-user.js
+++ middleware/auth-user.js
@@ -2,26 +2,32 @@
 
 //Defining and exporting a middleware function
 'use strict';
-//importing basic-auth module
+//importing basic-auth module & User model
 const auth = require('basic-auth');
+const bcrypt = require('bcrypt');
+const { User } = require('../models');
 // Middleware to authenticate the request & exporting it so we can import it from within another module.
 exports.authenticateUser = async (req, res, next) => {
-  //   // Parse the user's credentials from the Authorization header.
+// Parse the user's credentials from the Authorization header to see if user's key is association with any others in database
+  const credentials = auth(req);
+  if (credentials) {
+       // Attempt to retrieve the user from the data store
+    const user = await User.findOne({ where: {username: credentials.name} });
+    if (user) {
+        const authenticated = bcrypt
+        // Use the bcrypt npm package to compare the user's password
+        .compareSync(credentials.pass, user.confirmedPassword);
+        // If the passwords match...compareSync() returns true, or false if they don't
+        if (authenticated) { 
 
-  // If the user's credentials are available...
-     // Attempt to retrieve the user from the data store
-     // by their username (i.e. the user's "key"
-     // from the Authorization header).
+        }
+    }
+}
+  next();
+};
+ 
 
-  // If a user was successfully retrieved from the data store...
-     // Use the bcrypt npm package to compare the user's password
-     // (from the Authorization header) to the user's password
-     // that was retrieved from the data store.
-
-  // If the passwords match...
-     // Store the retrieved user object on the request object
-     // so any middleware functions that follow this middleware function
-     // will have access to the user's information.
+  
 
   // If user authentication failed...
      // Return a response with a 401 Unauthorized HTTP status code.
@@ -29,7 +35,6 @@ exports.authenticateUser = async (req, res, next) => {
   // Or if user authentication succeeded...
      // Call the next() method.
 
-  next();
-};
+
 //the next() function passes execution to the next middleware
 
